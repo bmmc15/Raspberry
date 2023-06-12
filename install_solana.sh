34c82a0b91794bac79a2d3d17ac42d673dc9112e
bmmc15 <a46054@alunos.isel.pt>
2023-06-12 20:19:48 +0100

    install solana script is now a install node script

--- install_solana.sh
+++ install_solana.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 # Install Node.js and npm
 echo "Installing Node..."
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+echo "Checking if file exists..."
+# Check if node-v18.16.0-linux-arm64.tar.xz file exists in the current directory
+if [ ! -f node-v18.16.0-linux-arm64.tar.xz ]; then
+    echo "node-v18.16.0-linux-arm64.tar.xz file not found in the current directory."
+    echo "Please copy the file to this directory and try again."
+    exit 1
+fi
 
-echo "Installing TypeScript and Solana."
-# Install TypeScript, and Solana
-sudo npm install -g typescript @solana/web3.js
+# Extract Node
+tar -xf node-v18.16.0-linux-arm64.tar.xz
+# Install Node
+cd node-v18.16.0-linux-arm64
+sudo cp -R * /usr/local/
 
-echo "Installing Solana finished!"
+# Add to the end of the bashrc file this export
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+# Apply the changes to the current terminal session
+source ~/.bashrc
+
+## TODO: call here the future script of the solana wallet, for now this script just installs Node
+echo "Installing Node finished!"
